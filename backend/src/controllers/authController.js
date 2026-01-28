858c159f79103e5f47540f2530332272a2741b88
Nikita Kostenko <nikkost.ua@gmail.com>
2026-01-28 20:38:11 +0200

    feat: Add user login API with JWT token

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,6 +2,8 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
+// Register
 exports.register = async (req, res) => {
   try {
     const { name, email, password, role, position } = req.body;
@@ -25,6 +27,46 @@ exports.register = async (req, res) => {
     await newUser.save();
 
     res.status(201).json({ message: 'User created successfully!' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+
+// Login
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+ 
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Wrong email or password' });
+    }
+
+   
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Wrong email or password' });
+    }
+
+    // Gennerate JWT
+    const token = jwt.sign(
+      { userId: user._id, role: user.role }, 
+      process.env.JWT_SECRET,                
+      { expiresIn: '1d' }                    
+    );
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+        email: user.email
+      }
+    });
+
   } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
